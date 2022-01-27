fd2eadd38a0473b3da0a5a982e6454094ba78b08
airmanxtw <airmanx@stust.edu.tw>
2022-01-27 18:26:28 +0800

    test

--- index.test.js
+++ index.test.js
@@ -1,14 +1,17 @@
 const getDataUrl = require("./index.js");
 
 test('go test', done => {
-    callback = (data) => {
-        //expect(data).toEqule("data:text/plain;base64,dGVzdCB0ZXh0");
-        console.log('start...');
-        expect(data).toBe(1);
-        done();
-    }
+    // callback = (data) => {
+    //     //expect(data).toEqule("data:text/plain;base64,dGVzdCB0ZXh0");
+    //     console.log('start...');
+    //     expect(data).toBe(1);
+    //     done();
+    // }
 
-    //var oMyBlob = new Blob(["test text"], { type: 'text/plain' });
-    var file = new File(["test text"], "abc.txt",{ type: 'text/plain' });
+   
+
+    var oMyBlob = new Blob(["test text"], { type: 'text/plain' });
+    var file = new File(["test text"], "abc.txt",{ type: 'text/plain' });    
+
     getDataUrl(file, callback);
 });
